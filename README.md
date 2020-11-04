a33485dbadfd4b59bb14dfbe680d38d1480817e6
Kousuke Kawahira <kawahira@binzume.net>
2020-11-04 17:53:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 動作確認用アセット
+
+https://github.com/binzume/vr-workspace の動作確認用のファイル群．
+
+利用時はそれぞれのモデルのライセンスに従ってください．
+
+- AliciaSolid.vrm: ニコニ立体ちゃん https://3d.nicovideo.jp/works/td14712
+- bunny.mp4: Big Buck Bunny  https://peach.blender.org/
+- startmap_360.jpg: NASA/Goddard Space Flight Center Scientific Visualization Studio https://svs.gsfc.nasa.gov/vis/a000000/a003500/a003572/
+- NamibDune_360.png: NASA / JPL / MSSS / Seán Doran https://informal.jpl.nasa.gov/museum/360-video
---
+++ mklist.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const targets = ['images', 'videos', 'models'];
+const types = {
+    '.vrm': 'application/vrm',
+    '.jpg': 'image/jpeg',
+    '.png': 'image/png',
+    '.mp4': 'video/mp4',
+};
+
+let files = [];
+function traverse(file) {
+    let stats = fs.statSync(file);
+    if (stats.isDirectory()) {
+        fs.readdirSync(file).map(f => file + '/' + f).forEach(traverse);
+    } else if (types[path.extname(file)] && !file.match(/_thumb\.jpg$/)) {
+        let thumb = file.replace(/\.[^\.]+$/, '_thumb.jpg');
+        let thumbExists = fs.existsSync(thumb);
+        files.push({
+            name: path.basename(file),
+            path: file,
+            url: file,
+            size: stats.size,
+            updatedTime: stats.mtime,
+            type: types[path.extname(file)],
+            thumbnailUrl: thumbExists ? thumb : null,
+        });
+    }
+}
+targets.forEach(traverse);
+console.log(JSON.stringify({ name: 'Demo assets', items: files }));
